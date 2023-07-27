44c1b70b3ba90663ebb0b30f6b5ae73c04aa0b9f
ceceliaroque <2757068168@qq.com>
2023-07-27 12:44:30 +0800

    上传下载，待修改

--- my_vue_frontend/vue.config.js
+++
@@ -1,5 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  lintOnSave: false, // eslint-loader 是否在保存的时候检查
-})
--- signature_and_hash/README.md
+++
@@ -1,5 +0,0 @@
-# 数字签名及完整性验证
-
-运行方法：
-
-- 进入signature_and_hash目录，运行main.py
